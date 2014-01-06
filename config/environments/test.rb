bf416d22598bf3107109b81286c116b17ebf1878
Lactor <machado007@gmail.com>
2014-01-06 12:08:52 -0500

    Finished signup

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,7 @@ TutSampleApp::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  # Speed up hashing process for creation
+  ActiveModel::SecurePassword.min_cost = true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 TutSampleApp::Application.routes.draw do
-  get "users/new"
+
+  resources :users
 
   root 'static_pages#home' 
   match '/signup', to: 'users#new', via: 'get'
