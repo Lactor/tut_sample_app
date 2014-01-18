59f0e87d1be046b3b221d9f0f31853c04d0c52aa
Lactor <machado007@gmail.com>
2014-01-17 22:17:02 -0500

    Finish user controls

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,7 +6,7 @@ module SessionsHelper
     user.update_attribute(:remember_token, User.encrypt(remember_token))
     self.current_user = user
   end
-
+  
   def signed_in?
     !current_user.nil?
   end
@@ -16,9 +16,27 @@ module SessionsHelper
     @current_user ||= User.find_by( remember_token: remember_token)
   end
   
-  def sign_out
-    current_user.update_attribute(:remember_token, User.encrypt(User.new_remember_token))
-    cookies.delete(:remember_token)
-    self.current_user = nill
+  def current_user?(user)
+    user == current_user
   end
+
+   def sign_out
+     current_user.update_attribute(:remember_token, User.encrypt(User.new_remember_token))
+     cookies.delete(:remember_token)
+     self.current_user = nil
+   end
+
+   def current_user=(user)
+     @current_user = user
+   end
+
+   def redirect_back_or(default)
+     redirect_to(session[:return_to] || default)
+     session.delete(:return_to)
+   end
+  
+   def store_location
+     session[:return_to] = request.url if request.get?
+   end
+
 end
